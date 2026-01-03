e5f1fc63cee3e54a0baa9409dfe8ac092cadf9c0
Diana_Surugiu <diana.surugiu19@yahoo.com>
2026-01-03 19:39:10 +0200

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = r"C:/Users/User/Pictures/Screenshots"
+destination_dir = r"C:/Users/User/Desktop/Backups"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    if os.path.exists(dest_dir):
+        print(f"Backup already exists: {dest_dir}")
+        return
+
+    shutil.copytree(source, dest_dir)
+    print(f"Folder copied to: {dest_dir}")
+
+schedule.every().day.at("10:15").do(copy_folder_to_directory, source_dir, destination_dir)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
